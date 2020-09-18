a993ab111e3e4c54fe677434876b43dcc20777c8
eeyorep33 <eeyorep33@gmail.com>
2020-09-17 20:12:33 -0400

    adding DB config

--- src/util/database.js
+++ src/util/database.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
 
-const sequelize = new Sequelize(process.env.SCHEMA, process.env.USER_NAME, process.env.PASSWORD, {
+const sequelize = new Sequelize('mysql://b9789f3888e97b:d2d16f70@us-cdbr-east-02.cleardb.com/heroku_ee08dbd47dd791a?reconnect=true', {
     dialect: 'mysql', 
-    host: 'localhost'
+    host: 'us-cdbr-east-04.cleardb.com'
 });
 
 

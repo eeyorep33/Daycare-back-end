ec192e9f8f9491cef33f0217e21dacc650c42cbf
eeyorep33 <eeyorep33@gmail.com>
2020-09-18 08:37:07 -0400

    changing db string

--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
 
-const sequelize = new Sequelize('mysql://b9789f3888e97b:d2d16f70@us-cdbr-east-02.cleardb.com/heroku_ee08dbd47dd791a?reconnect=true', {
+const sequelize = new Sequelize('mysql://b282e0fd009417:f1515863@us-cdbr-east-02.cleardb.com/heroku_a7b708b98d88c66?reconnect=true', {
     dialect: 'mysql', 
-    host: 'us-cdbr-east-04.cleardb.com'
+    host: 'us-cdbr-east-02.cleardb.com'
 });
 
 
